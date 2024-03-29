1877cac80fcd941f721f3aad2ab660bc9a40323d
Mauriciovillarreal <134817395+Mauriciovillarreal@users.noreply.github.com>
2024-03-29 18:03:12 -0300

    Add files via upload
    sumando el "limit" ya que se me paso

--- app.js
+++ app.js
@@ -7,16 +7,29 @@ const productManager = new ProductManager('./products.json');
 const app = express()
 
 app.get('/products', async (req, res) => {
-    const products = await productManager.getProducts();
-    res.send(products);
+    try {
+        const limit = req.query.limit;
+        const products = await productManager.getProducts();
+        if (limit) {
+            const limitedProducts = products.slice(0, parseInt(limit));
+            return res.json(limitedProducts);
+        }
+        res.json(products);
+    } catch (error) {
+        console.log(error)
+    }
 });
 
- app.get('/products/:pid', async (req, res) => {
-     const { pid } = req.params;
-     const productos = await productManager.getProducts();
-     const product =  productos.find(p => p.id === parseInt(pid))
-     res.send(product);
- });
+app.get('/products/:pid', async (req, res) => {
+    try {
+        const { pid } = req.params;
+        const productos = await productManager.getProducts();
+        const product = productos.find(p => p.id === parseInt(pid))
+        res.send(product);
+    } catch (error) {
+        console.log(error)
+    }
+});
 
 app.listen(8080, err => {
     if (err) console.log(err)
